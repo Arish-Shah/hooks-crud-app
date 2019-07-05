520924038d0aebcbf8b16e37dac55610b587ebce
arizh <arish0246@gmail.com>
2019-07-05 14:01:00 +0530

    Updates

---
+++ precache-manifest.f79aeedaa36c7dad6587face1408fbad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4022311dd8c7b7c1a6c3e76c8be9e24",
+    "url": "/hooks-crud-app/index.html"
+  },
+  {
+    "revision": "d4f8991a5148afdb60b9",
+    "url": "/hooks-crud-app/static/css/main.fcefbb83.chunk.css"
+  },
+  {
+    "revision": "77abc3b513c1762f8a17",
+    "url": "/hooks-crud-app/static/js/2.87fef826.chunk.js"
+  },
+  {
+    "revision": "d4f8991a5148afdb60b9",
+    "url": "/hooks-crud-app/static/js/main.30f9e9a5.chunk.js"
+  },
+  {
+    "revision": "0bfe9e1306f61b77f0f1",
+    "url": "/hooks-crud-app/static/js/runtime~main.2208486a.js"
+  }
+]);
